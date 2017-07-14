8eafcd8ae2fc556f177ff5ec8d87474d6e11d3e3
Brandon Baker <brandonbaker40@icloud.com>
2017-07-13 20:28:04 -0400

    adds API and token access

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+25.times do
+   User.create!(
+   username:     Faker::Internet.user_name,
+   email:    Faker::Internet.free_email,
+   password: Faker::Internet.password(8)
+   )
+ end
+ #users = User.all
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
