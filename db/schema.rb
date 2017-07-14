aa1b4ecb570fd0469e04f57771a91ba59e7807d6
Brandon Baker <brandonbaker40@icloud.com>
2017-07-13 20:41:29 -0400

    adds lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713234214) do
+ActiveRecord::Schema.define(version: 20170714003747) do
+
+  create_table "lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
